30c63679ae7df526fa07557381985b8cf8a6b4be
Diana Fisher <diana.fisher@gmail.com>
2017-04-14 13:57:54 -0700

    Install react and other project dependencies

---
+++ app/components/App.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+class App extends React.Component {
+  render() {
+    <div className="container">
+      <h1>Hello, world!</h1>
+    </div>
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
+var App = require('./components/App');
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
