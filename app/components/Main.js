03d2256439384263c2024ae32495dcc3a9812798
Diana Fisher <diana.fisher@gmail.com>
2017-04-14 16:22:48 -0700

    Add styling to Main component

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,25 @@
-var React = require('react');
+import React, { Component } from 'react';
+
+const styles = {
+  content: {
+    backgroundImage: 'url(app/images/pattern.svg)',
+    backgroundSize: 'cover',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%'
+  },
+  white: {
+    color: 'white'
+  }
+}
 
 class Main extends React.Component {
   render() {
     return (
-      <div className='main-container'>
-        <h1>Hello, Main!</h1>
+      <div className='main-container' style={styles.content}>
+        <h1 style={styles.white}>Hello, Main!</h1>
       </div>
 
     )
--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
 var App = require('./components/App');
+require('./index.css');
 
 ReactDOM.render(
   <App />,
