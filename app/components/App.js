75f4510e546921bec6274087cd0f2580108dbdd5
Diana Fisher <diana.fisher@gmail.com>
2017-04-14 14:15:39 -0700

    Configure webpack and get initial App component running

--- app/components/App.js
+++ app/components/App.js
@@ -2,8 +2,12 @@ var React = require('react');
 
 class App extends React.Component {
   render() {
-    <div className="container">
-      <h1>Hello, world!</h1>
-    </div>
+    return (
+      <div className="container">
+        <h1>Hello, world!</h1>
+      </div>  
+    )
   }
 }
+
+module.exports = App;
