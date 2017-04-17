4076b587ce556500bf8a3f7005098b46f0597b50
Diana Fisher <diana.fisher@gmail.com>
2017-04-16 18:25:15 -0700

    Add lodash to dependencies.  Group forecast results by day.

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import moment from 'moment';
+import _ from 'lodash';
 import {getForecast} from '../utils/api';
 
 class Forecast extends React.Component {
 
   componentDidMount() {
     console.log('Forecast component mounted');
+
     getForecast('Santa Cruz, CA')
       .then(function (data) {
         console.log(data);
         const list = data.list;
 
-        list.forEach(item => {
-          console.log(item);
-          // console.log(item.dt_txt);
-          // use momentjs to get the local date
+        // list.forEach(item => {
+        //   console.log(item);
+        //   // console.log(item.dt_txt);
+        //   // use momentjs to get the local date
+        //   const date = moment.utc(item.dt_txt, 'YYYY-MM_DD HH:mm:ss').local();
+        //   console.log(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+        //   item.date = date;
+        //   console.log(date.day());
+        // });
+        //
+        // console.log(list);
+
+        // group the results by day
+        let groupedResults = _.groupBy(list, (item) => {
           const date = moment.utc(item.dt_txt, 'YYYY-MM_DD HH:mm:ss').local();
-          console.log(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
-          console.log(date.day());
-        })
+          item.date = date;
+          return date.day();
+        });
+
+        console.log(groupedResults);
+        console.log(groupedResults[0]);
+        for (var r in groupedResults) {
+          console.log(r + ' = ' + groupedResults[r]);
+          let items = groupedResults[r];
+          console.log(items);
+
+          items.forEach(item => {
+            const date = item.date;
+            console.log(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+          })
+        }
 
       });
   }
