6dbf286bd8937ee24df8685c39bf38af9c1792fc
Diana Fisher <diana.fisher@gmail.com>
2017-04-16 17:38:38 -0700

    Parse UTC date into local time using momentjs.

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -6,20 +6,18 @@ class Forecast extends React.Component {
 
   componentDidMount() {
     console.log('Forecast component mounted');
-    getForecast('Orlando')
+    getForecast('Santa Cruz, CA')
       .then(function (data) {
         console.log(data);
         const list = data.list;
 
         list.forEach(item => {
-          console.log(item.dt_txt);
-          // split each item into date and time
-          let parts = item.dt_txt.split(' ');
-          console.log(parts);
-          let date = new Date(parts[0]);
-          console.log(date.getUTCDay());
-          let day = moment(item.dt_txt).format('dddd, MMMM Do YYYY, h:mm:ss a');
-          console.log(day);
+          console.log(item);
+          // console.log(item.dt_txt);
+          // use momentjs to get the local date
+          const date = moment.utc(item.dt_txt, 'YYYY-MM_DD HH:mm:ss').local();
+          console.log(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+          console.log(date.day());
         })
 
       });
